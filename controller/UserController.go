d01c74925aaafc0c346301e3a16ab494971b9391
Caesar454905970 <454905970@qq.com>
2021-10-14 16:23:21 +0800

    new

---
+++ controller/UserController.go
@@ -0,0 +1,213 @@
+package controller
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/dchest/captcha"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+
+	"jkdev.cn/api/common"
+	"jkdev.cn/api/dto"
+	"jkdev.cn/api/model"
+	"jkdev.cn/api/response"
+	"jkdev.cn/api/util"
+)
+
+type CaptchaResponse struct {
+	CaptchaId string `json:"captchaId"` //验证码Id
+	ImageUrl  string `json:"imageUrl"`  //验证码图片url
+}
+
+//1.获取验证码
+func GetCaptchaId(ctx *gin.Context) {
+	length := captcha.DefaultLen
+	captchaId := captcha.NewLen(length)
+	var captcha CaptchaResponse
+	captcha.CaptchaId = captchaId
+	captcha.ImageUrl = "/captcha/" + captchaId + ".png"
+	ctx.JSON(http.StatusOK, captcha)
+}
+
+//2.获取验证码图片
+func GetCaptchaPng(ctx *gin.Context) {
+	var requestUser = CaptchaResponse{}
+	captchaId := ctx.Bind(&requestUser)
+	fmt.Println(captchaId) 
+	response.Success(ctx, gin.H{"CaptchaId": requestUser.CaptchaId, "imageUrl": requestUser.ImageUrl}, "验证码获取成功")
+}
+
+func Register(ctx *gin.Context) {
+
+	DB := common.GetDB()
+	//使用map获取请求参数
+	var requestUser = model.User{}
+	ctx.Bind(&requestUser)
+
+	//获取参数
+	name := requestUser.Name
+	telephone := requestUser.Telephone
+	password := requestUser.Password
+	//数据验证
+	fmt.Println(telephone, "手机号码长度", len(telephone))
+	if len(telephone) != 11 {
+		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "手机号必须为11位")
+		return
+	}
+	if len(password) < 6 {
+		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "密码不能少于6位")
+		return
+	}
+	if len(name) == 0 {
+		name = util.RandString(10)
+	}
+
+	//判断手机号码是否存在
+	if isTelephoneExists(DB, telephone) {
+		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "用户已经存在")
+		return
+	}
+
+	//创建用户
+	hasePassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		response.Response(ctx, http.StatusUnprocessableEntity, 500, nil, "加密失败")
+		return
+	}
+
+	newUser := model.User{
+		Name:      name,
+		Telephone: telephone,
+		Password:  string(hasePassword),
+	}
+	DB.Create(&newUser)
+
+	//返回结果
+	//发放token
+	token, err := common.ReleaseToken(newUser)
+	if err != nil {
+		response.Response(ctx, http.StatusUnprocessableEntity, 500, nil, "系统异常")
+		log.Printf("token generate error: %v", err)
+		return
+	}
+
+	//返回结果
+	response.Success(ctx, gin.H{"token": token}, "注册成功")
+}
+
+func Login(ctx *gin.Context) {
+	DB := common.GetDB()
+	//获取数据
+	//使用map获取请求参数
+	var requestUser = model.User{}
+	ctx.Bind(&requestUser)
+
+	//获取参数
+	telephone := requestUser.Telephone
+	password := requestUser.Password
+	//数据验证
+	fmt.Println(telephone, "手机号码长度", len(telephone))
+	if len(telephone) != 11 {
+		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "手机号必须为11位")
+		return
+	}
+	if len(password) < 6 {
+		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "密码不能少于6位")
+		return
+	}
+
+	//判断手机号是否存在
+	var user model.User
+	DB.Where("telephone = ?", telephone).First(&user)
+	if user.ID == 0 {
+		response.Response(ctx, http.StatusUnprocessableEntity, 400, nil, "用户不存在")
+		return
+	}
+
+	//判断密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		response.Response(ctx, http.StatusBadRequest, 400, nil, "密码错误")
+		return
+	}
+
+	//发放token
+	token, err := common.ReleaseToken(user)
+	if err != nil {
+		response.Response(ctx, http.StatusUnprocessableEntity, 500, nil, "系统异常")
+		log.Printf("token generate error: %v", err)
+		return
+	}
+
+	//返回结果
+	response.Success(ctx, gin.H{"token": token}, "登录成功")
+}
+
+func Info(ctx *gin.Context) {
+	user, _ := ctx.Get("user")
+	ctx.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"data": gin.H{"user": dto.ToUserDto(user.(model.User))},
+	})
+}
+
+func isTelephoneExists(db *gorm.DB, telephone string) bool {
+	var user model.User
+	db.Where("telephone = ?", telephone).First(&user)
+	if user.ID != 0 {
+		return true
+	}
+	return false
+}
+
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	dir, file := path.Split(r.URL.Path)
+	ext := path.Ext(file)
+	id := file[:len(file)-len(ext)]
+	fmt.Println("file : " + file)
+	fmt.Println("ext : " + ext)
+	fmt.Println("id : " + id)
+	if ext == "" || id == "" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Println("reload : " + r.FormValue("reload"))
+	if r.FormValue("reload") != "" {
+		captcha.Reload(id)
+	}
+	lang := strings.ToLower(r.FormValue("lang"))
+	download := path.Base(dir) == "download"
+	if Serve(w, r, id, ext, lang, download, captcha.StdWidth, captcha.StdHeight) == captcha.ErrNotFound {
+		http.NotFound(w, r)
+	}
+}
+
+func Serve(w http.ResponseWriter, r *http.Request, id, ext, lang string, download bool, width, height int) error {
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	w.Header().Set("Pragma", "no-cache")
+	w.Header().Set("Expires", "0")
+
+	var content bytes.Buffer
+	switch ext {
+	case ".png":
+		w.Header().Set("Content-Type", "image/png")
+		captcha.WriteImage(&content, id, width, height)
+	case ".wav":
+		w.Header().Set("Content-Type", "audio/x-wav")
+		captcha.WriteAudio(&content, id, lang)
+	default:
+		return captcha.ErrNotFound
+	}
+
+	if download {
+		w.Header().Set("Content-Type", "application/octet-stream")
+	}
+	http.ServeContent(w, r, id+ext, time.Time{}, bytes.NewReader(content.Bytes()))
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,30 @@
+module jkdev.cn/api
+
+go 1.14
+
+require (
+	github.com/dchest/captcha v0.0.0-20200903113550-03f5f0333e1f // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-contrib/sessions v0.0.3
+	github.com/gin-gonic/gin v1.7.4
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
+	github.com/jinzhu/gorm v1.9.12
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/mojocn/base64Captcha v1.2.2
+	github.com/satori/go.uuid v1.2.0
+	github.com/spf13/viper v1.6.3
+	github.com/ugorji/go v1.2.6 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
+	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
